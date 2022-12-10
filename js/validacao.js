76f414dbe9c8f2bd31f3c14981cb2766ece3a404
Paulo Scarano <scaranopaulo@gmail.com>
2022-12-10 18:07:03 -0300

    Mostrando mensagem de erro

--- js/validacao.js
+++ js/validacao.js
@@ -1,30 +1,58 @@
 export function valida(input) {
-    const tipoDeInput = input.dataset.tipo
+  const tipoDeInput = input.dataset.tipo;
 
-    if (validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input);
-    }
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalido");
+  } else {
+    input.parentElement.classList.add("input-container--invalido");
+  }
 }
+
+const mensagensDeErro = {
+  nome: {
+    valueMissing: "O campo nome não pode estar vazio",
+  },
+  email: {
+    valueMissing: "O campo de email não pode estar vazio",
+    typeMismatch: "O email digitado não é válido",
+  },
+  senha: {
+    valueMissing: "Ocampo de senha não pode estar vazio",
+    patternMismatch:
+      "A senha deve conter entre 6 e 12 caracteres, deve conter pelo menos uma letra maiuscula e uma minuscula, e não deve conter símbolos!",
+  },
+  dataNascimento: {
+    valueMissing: "O Campo de data de nascimento não pode estar vazio",
+    customError: "Você deve ser maior de 18 anos para se cadastrar"
+  }
+};
 
 const validadores = {
-    dataNascimento:input => validaNascimento(input)
-}
+  dataNascimento: (input) => validaNascimento(input),
+};
 
 function validaNascimento(input) {
-    const dataRecebida = new Date(input.value);
-    let mensagem = '';
+  const dataRecebida = new Date(input.value);
+  let mensagem = "";
 
-    if(!maiorQue18(dataRecebida)) {
-        mensagem = 'Você deve ser maior de 18 anos para se cadastrar';
-    }
+  if (!maiorQue18(dataRecebida)) {
+    mensagem = "Você deve ser maior de 18 anos para se cadastrar";
+  }
 
-    input.setCustomValidity(mensagem)
+  input.setCustomValidity(mensagem);
 }
 
 function maiorQue18(data) {
-    const dataAtual = new Date();
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+  const dataAtual = new Date();
+  const dataMais18 = new Date(
+    data.getUTCFullYear() + 18,
+    data.getUTCMonth(),
+    data.getUTCDate()
+  );
 
-    return dataMais18 <= dataAtual;    
-
+  return dataMais18 <= dataAtual;
 }
