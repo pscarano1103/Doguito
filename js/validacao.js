f00991c6a3748d1ff40140717716d8bd0473df47
Paulo Scarano <scaranopaulo@gmail.com>
2022-12-13 18:38:39 -0300

    Validação de CPF concluída

--- js/validacao.js
+++ js/validacao.js
@@ -84,7 +84,7 @@ function validaCPF(input) {
   const cpfFormatado = input.value.replace(/\D/g, '')
   let mensagem =''
 
-  if(!checaCPFRepetido(cpfFormatado)) {
+  if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
     mensagem = 'O CPF Digitado não é válido.'
   }
 
@@ -115,4 +115,37 @@ function checaCPFRepetido (cpf) {
   })
 
   return cpfValido
+}
+
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+
+    if(multiplicador >= 12) {
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigito = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigito[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+
+}
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11)
 }
